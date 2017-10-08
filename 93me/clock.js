b814a3ec82ffc8e941caef5bf5342d63c10530cc
闫锦涛 <13651168620@163.com>
2017-10-08 22:51:16 +0800

    Signed-off-by: 闫锦涛 <13651168620@163.com>

--- 93me/clock.js
+++ 93me/clock.js
@@ -2,7 +2,7 @@
 document.write('<div id="htright" class="float_r align_r"></div>');
 function show_student_time(){ 
 window.setTimeout("show_student_time()", 1000); 
-BirthDay=new Date("2017/08/08 20:34");
+BirthDay=new Date("2017/10/9 20:34");
 today=new Date(); 
 timeold=(today.getTime()-BirthDay.getTime());
 sectimeold=timeold/1000 
@@ -15,6 +15,6 @@ hrsold=Math.floor(e_hrsold);
 e_minsold=(e_hrsold-hrsold)*60; 
 minsold=Math.floor((e_hrsold-hrsold)*60); 
 seconds=Math.floor((e_minsold-minsold)*60); 
-	htright.innerHTML='<font  color=#00ffff>亲爱的老婆，我们在一起已经'+daysold+'天 '+hrsold+'时'+minsold+'分'+seconds+'秒</font><DIV style="DISPLAY: none"><script src="http://s9.cnzz.com/z_stat.php?id=1253206362&web_id=1253206362" language="JavaScript"></script><script language="javascript" type="text/javascript" src="http://js.users.51.la/17292473.js"></script>';
+	htright.innerHTML='<font  color=#00ffff>我们在一起已经'+daysold+'天 '+hrsold+'时'+minsold+'分'+seconds+'秒</font><DIV style="DISPLAY: none"><script src="http://s9.cnzz.com/z_stat.php?id=1253206362&web_id=1253206362" language="JavaScript"></script><script language="javascript" type="text/javascript" src="http://js.users.51.la/17292473.js"></script>';
 	} 
 show_student_time();
